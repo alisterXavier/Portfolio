df13173b3b10ab84ab8951ed462850fd17b5d5df
Alister <xavieralister153@gmail.com>
2022-10-16 17:24:41 +0400

    styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,6 @@
 import { createContext, createRef, useEffect, useRef, useState } from "react";
 import "../styles/globals.css";
 import "../styles/styles.sass";
-import ReactGA from 'react-ga';
-
-const TRACKING_ID = "G-RH02VTQ8K5"; // OUR_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
-
 export const ContactApi = createContext();
 export const Screen = createContext();
 export const Nav = createContext();
@@ -17,7 +12,6 @@ function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
     setScreen(document.documentElement.clientWidth);
-    ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
   return (
     <Screen.Provider value={screen}>
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,23 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { useEffect } from "react";
 
 export default function Document() {
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+
+    gtag("config", "G-RH02VTQ8K5");
+  },[]);
   return (
     <Html>
       <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-RH02VTQ8K5"
+        ></script>
         <link
           href="https://fonts.cdnfonts.com/css/hello-denver-display"
           rel="stylesheet"
