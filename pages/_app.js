4db4a2bc933908ccd17b085142ceedb1bf6cff44
Alister <xavieralister153@gmail.com>
2022-10-16 17:13:01 +0400

    styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import { createContext, createRef, useEffect, useRef, useState } from "react";
 import "../styles/globals.css";
 import "../styles/styles.sass";
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = "G-RH02VTQ8K5"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 export const ContactApi = createContext();
 export const Screen = createContext();
@@ -13,6 +17,7 @@ function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
     setScreen(document.documentElement.clientWidth);
+    ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
   return (
     <Screen.Provider value={screen}>
