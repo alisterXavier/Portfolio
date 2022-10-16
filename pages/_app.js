3b1e725cc0d9a922789927fd16c4d07f7ffd1ee2
Alister <xavieralister153@gmail.com>
2022-10-16 17:29:29 +0400

    styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import { createContext, createRef, useEffect, useRef, useState } from "react";
 import "../styles/globals.css";
 import "../styles/styles.sass";
+import ReactGA from "react-ga";
+
+const TRACKING_ID = "G-RH02VTQ8K5"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+
 export const ContactApi = createContext();
 export const Screen = createContext();
 export const Nav = createContext();
@@ -12,14 +17,15 @@ function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
     setScreen(document.documentElement.clientWidth);
+    ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
   return (
     <Screen.Provider value={screen}>
-        <Nav.Provider value={[navState, setNavState]}>
-          <ContactApi.Provider value={[contactState, setContactState]}>
-            <Component {...pageProps} />
-          </ContactApi.Provider>
-        </Nav.Provider>
+      <Nav.Provider value={[navState, setNavState]}>
+        <ContactApi.Provider value={[contactState, setContactState]}>
+          <Component {...pageProps} />
+        </ContactApi.Provider>
+      </Nav.Provider>
     </Screen.Provider>
   );
 }
