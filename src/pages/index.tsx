e60e1f020692fb028026e6dc71f81ece42c7578c
Alister <xavieralister153@gmail.com>
2023-02-22 20:33:53 +0400

    favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ import Project from "./Components/Projects/Project";
 import { ProjectDataInterface } from "../types/types";
 import colors from "@a/data/colors.json";
 import { useRandomNumber } from "../Hooks/randomNumber";
+import Head from "next/head";
 
 export default function Home() {
   const isSmallScreen = useSmallDeviceSize();
@@ -69,6 +70,29 @@ export default function Home() {
 
   return (
     <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <div className={`main ${navToggle && "navbar-active"}`}>
         <Navbar
           navToggle={navToggle}
